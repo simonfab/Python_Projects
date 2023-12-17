eef32522e4216579c026eff8778bcbe080339a90
Simon Kinsey <Simontkinsey@yahoo.co.uk>
2023-12-17 15:09:21 +0000

    update to finance calcs

--- finance_calculators.py
+++ finance_calculators.py
@@ -4,33 +4,40 @@ Task 05 - Capstone Project - finance calcuators
 Student details:
 #student_name = "Simon Kinsey" 
 #student_number = "SK23110011962"
-'''
 
-'''
-pseudo code
-
+Pseudo code
 import libraries
     math
 
-define functions
+define functions:
     get choice
         validate including making allowances for caps etc
         call appropriate function
-    Get data and validate
-        validate int 
-    simple interest return and return message
-    compound interest return and return message
-    bond/mortgage repayment calculator and return message
+            end
+            bond
+                call bond input and calc message
+            investment
+                determine if they want simple or compound    
+                call correct interest function
+        end option
+            set continue to false
+            set message to goodbye
+    bond option input
+        get data and validate (positive floats or ints as approp)
+        call bond calc function
+    simple interest input
+        get data and validate (positive floats or ints as approp) could this be the same funtion as for bond?
+        call simple interest calc
+    compound interest input
+        get data and validate (positive floats or ints as approp) could this be the same funtion as for bond and simple?
+        call compound interest calc
+    bond calculator
+        basic forumla = repayment = (i*p)/(1-(1+i)**(-n))
+    simple interest calculator
+        basic forumla = A = P*(1+r*t)
+    compound interest calculator
+        basic forumla = A=P*math.pow(((1+r),t)
 
-    
-Set up while loop
-    get choice function
-    get data input
-    print output
-
-
-
-collect user option and validate that choice matches an option
 if investment:
     ask for inputs of 
     rate of interest (float?)
@@ -49,10 +56,29 @@ if bond:
     do the calculations using the defined function and calculate return messages
     printout the answer - assume - no of months to repay, total repaid, interest paid
     perhaps do a comparison option
-print messages
-Ask if they want to do another
-    Set variable if they want to end
-    print thank you for using system
-    else carry on and do again
+print messages'''
 
-'''
+# Initialise variables
+carry_on = True
+
+# Define functions
+def print_messages(message):
+    print(message)
+
+print("Welcome to the finance calculators - enter your selection")
+
+while carry_on:
+    init_choice = input("Please select from \"bond\", \"savings\" or \"quit\" (to terminate program)")
+    print(init_choice)
+    final_choice = init_choice[1:3].lower()
+    print (f"final_choice is {final_choice})
+   
+    if init_choice == "quit":
+        carry_on = False
+    
+
+    #else:
+     #   print("You have entered an unrecognised string please try again or enter quit...")
+
+# The following statements are outside of the while loop
+print_messages(f"Thanks for using the system (carry_on = {carry_on})")  # This prints 'False' when 'quit is entered

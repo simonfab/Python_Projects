97b40bc1adeb82ce86e8fd3efb6e4c4f0d7b2936
Simon Kinsey <simontkinsey@yahoo.co.uk>
2023-12-21 13:00:01 +0000

    ch pt 2 to pattern

--- pattern.py
+++ pattern.py
@@ -4,7 +4,7 @@ Student details:
 #student_name = "Simon Kinsey" 
 #student_number = "SK23110011962"
 
-Pattern is
+Pattern required for 9 rows is
 *
 **
 ***
@@ -20,7 +20,14 @@ Pattern is
 Pseudo code
 Declare variables for:
 counter
-pattern 1
+pattern with 1 star
+ask the user for the number of rows to print (max interations +1 for loop)
+calculate the halfway point
+do the printing loop
+    decide what to print ie whether its met the half way point or not
+    to start decreasing the number of stars
+    print the row
+end
 '''
 
 
@@ -30,28 +37,25 @@ pattern ="*"
 
 
 # Opening message
-print ("welcome to the \"pattern\" printer:\n")
+print ("Welcome to the \"pattern\" printer:\n")
 
-rows = int (input ("how many rows to print?"))
-halfway = int (round (rows/2,0))
+# get user input
+# calculate halfway point
+rows = int (input ("How many rows would you like print? Odd numbers"
+                   "please, like 9!..."))
+halfway = int (round (rows/2,0)+1)
+print ()
 
-print(f"{rows} {halfway}\n",)
-
-
-
-                  
-
-
-#main loop to print the pattern
-
-
-
+# loop to print the pattern
 for counter in range (1,rows+1):
+
+    # if less than half way print the pattern for the
+    #numbers of rows so far:
     if counter <= halfway:
-        print (f"{pattern * counter} {counter} {halfway}")
+        print (f"{pattern * counter}")
+
+    # if its more than half way decrease the halfway counter
+    #and use that to print the number of stars:
     else:
         halfway -=1
-        print (f"{pattern * halfway} {counter} {halfway}")
-9
-
-''' use the half way counter in reverse'''
+        print (f"{pattern * halfway}")

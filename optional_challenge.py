3c668f49378bb9b5e27cfdf057224801806ec450
Simon Kinsey <Simontkinsey@yahoo.co.uk>
2024-01-06 21:25:32 +0000

    optional challenge adjusted for clarity

--- optional_challenge.py
+++ optional_challenge.py
@@ -5,7 +5,7 @@ Student details:
 #student_number = "SK23110011962"
 
 Notes:
-    pylint line lenght limit followed (100 chars)
+pylint line lenght limit followed (100 chars)
 
 Pseudo code
 Ask the user for a name
@@ -17,9 +17,8 @@ get the last word(s) to be last name
 print caclulated last name - this is where the logic problem is ..
 - it calculates last name as last 2 words but its too coarse to allow for all user entries so corrected version uses 
 just last word which allows for hyphens but not double barrelled names without hyphens
-
 '''
-
+#'''
 # incorrect version:
 name_string = Input("Please enter your full name...\n") # capital I on input
 print(ame_string)                                      # incorrect spelling of name_string variable - ie NameError - should be f string too
@@ -31,21 +30,20 @@ print ("Entire name is " + sep_string + name_len)  #(name_len is an int and need
 last_name_test = sep_string [sep_len+1]  # out of range error
 last_name = " ".join(sep_string [-2:])    # logic error - if the user enters a 2 word name or a "hyphenated last name" eg Winstone-smith it doesnt give the correct last name...
 print(last_name)
-
+#'''
 '''
 # Correct version:
 # The correct version appears below (but still with logic error which would require a lot more coding...)
 
 name_string = input("Please enter your full name...\n")
+name_string = name_string.title()
 print (f"Your full name is {name_string}")
 
 name_len = len (name_string)
 print (f"There are {name_len} characters in your name")
 
 sep_string = name_string.split(" ")
-
 sep_len = len(sep_string)
-
 print (f"Entire name is {sep_string} - {sep_len} words") # (name_len is an int and needs to be str)
 
 last_name = " ".join(sep_string [-1:])
